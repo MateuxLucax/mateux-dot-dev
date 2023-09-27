4f47f71af70d4e60a0c3f224e073e948a0f32c81
Mateus Lucas <mateuxlucax@gmail.com>
2023-09-26 23:21:10 -0300

    refac: migrate to Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Made with ❤️ by <a href="https://github.com/MateuxLucax" target="_blank">MateuxLucax</a> using <a href="https://svelte.dev">SvelteKit</a></p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 2.4rem;
+		width: 100%;
+		max-width: 76.8rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 1.2rem;
+		text-align: center;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 1.2rem 0;
+		}
+	}
+</style>
