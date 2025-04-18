9a31fa70237ad996325fd63d2a699ed4c8ab16ac
MateuxLucax <mateuxlucax@gmail.com>
2025-04-18 14:53:32 -0300

    refac: improve responsiveness

--- src/lib/TerminalHeader.svelte
+++ src/lib/TerminalHeader.svelte
@@ -1,4 +1,4 @@
-<header class="flex items-center justify-center relative bg-gray-50 dark:bg-gray-800 dark:border-gray-700 p-4 border-b-2 border-gray-200 mt-8 xl:mt-0">
+<header class="flex items-center justify-center relative bg-gray-50 dark:bg-gray-800 dark:border-gray-700 p-4 border-b-2 border-gray-200 mt-8 md:mt-0">
   <div class="absolute flex items-center gap-2 left-4 md:left-8">
     <span class="w-4 h-4 bg-red-400 rounded-full"></span>
     <span class="w-4 h-4 bg-yellow-400 rounded-full"></span>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
   let showXLImage = $state(false);
 
   onMount(() => {
-    const media = window.matchMedia('(min-width: 1280px)');
+    const media = window.matchMedia('(min-width: 768px)');
     showXLImage = media.matches;
 
     media.addEventListener('change', (event) => {
@@ -25,21 +25,21 @@
 <ActivityBar />
 
 <main
-  class="min-h-screen bg-base text-text font-mono flex xl:items-center xl:justify-center w-full"
+  class="min-h-screen bg-base text-text font-mono flex md:items-center md:justify-center w-full"
 >
   {#if showXLImage}
 		<enhanced:img
 			src="/static/background.jpg?blur=5" 
-			sizes="min(1280px, 100vw)"
+			sizes="min(768px, 100vw)"
 			alt="Forest with enormeous trees"
 			class="fixed inset-0 -z-10 w-full h-full object-cover"
 		/>
 	{/if}	
 	<section
-		class="xl:relative w-full max-w-7xl xl:max-w-4xl shadow-md border-2xl xl:rounded-xl overflow-hidden"
+		class="md:relative w-full max-w-7xl md:max-w-4xl shadow-md border-2xl md:rounded-xl overflow-hidden"
 	>
 		<TerminalHeader />
-		<section class="p-4 xl:p-8 h-full bg-white dark:bg-gray-800 dark:text-white terminal-body overflow-y-auto">
+		<section class="p-4 md:p-8 h-full bg-white dark:bg-gray-800 dark:text-white terminal-body overflow-y-auto">
   		<ul class="list-none gap-2 flex flex-col">
 				{@render children()}
 			</ul>
@@ -47,7 +47,7 @@
 </main>
 
 <style>
-	@media (min-width: 1280px) {
+	@media (min-width: 768px) {
 		.terminal-body {
 			max-height: var(--container-2xl);
 		}
