6d6090e1a323ad711b743826ff34ae45f70ff260
Mateus Brandt <mateuxlucax@gmail.com>
2025-06-16 20:59:28 -0300

    refac: finish my first blog post

--- src/routes/blog/posts/[slug]/+page.svelte
+++ src/routes/blog/posts/[slug]/+page.svelte
@@ -8,11 +8,12 @@
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
 </svelte:head>
 
 <article>
 	<header class="mb-8 flex flex-col gap-2">
-		<a href="/blog" target="_self" class="mb-6 underline text-blue-500 hover:text-blue-300">&larr; Back to posts</a>
+		<a href="/blog" target="_self" class="mb-6 underline text-blue-500 dark:text-blue-200 hover:text-blue-300 dark:hover:text-blue-100">&larr; Back to posts</a>
 		
 		<h1 class="text-3xl font-bold mb-2">{data.meta.title}</h1>
 		<p>Published at {formatDate(data.meta.date)}</p>
@@ -25,15 +26,15 @@
 		</section>
 	</header>
 
-	<div>
+	<section>
 		{#each data.meta.categories as category}
 			<span>&num;{category}</span>
 		{/each}
-	</div>
+	</section>
 
-	<div class="prose max-w-prose">
+	<section class="prose max-w-prose">
 		<data.content />
-	</div>
+	</section>
 </article>
 
 <style>
@@ -41,12 +42,18 @@
 		max-width: 100%;
 	}
 
+	* {
+		--tw-prose-links: var(--color-blue-600);
+		--tw-prose-code: var(--color-green-600);
+		--tw-prose-headings: var(--color-red-800);
+	}
+
 	@media (prefers-color-scheme: dark) {
 		* {
 			--tw-prose-body: var(--color-white);
-			--tw-prose-headings: var(--color-white);
-			--tw-prose-links: var(--color-blue-300);
-			--tw-prose-code: var(--color-gray-200);
+			--tw-prose-headings: var(--color-red-200);
+			--tw-prose-links: var(--color-blue-200);
+			--tw-prose-code: var(--color-green-200);
 		}
 	}
 </style>
